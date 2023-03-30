91b11462c4a83faed6c54d983d0b72dbcb4a83c4
nspin <pinchuk.nik@yandex.ru>
2023-03-31 00:56:32 +0300

    Переделал базовый layout

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,55 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <NavBar :mobile="mobile" />
+    <Suspense>
+      <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+          <Suspense timeout="0">
+            <template #default>
+              <component :mobile="mobile" :is="Component" :key="$route.path" />
+            </template>
+            <template #fallback>
+              <LoaderIcon />
+            </template>
+          </Suspense>
+        </template>
+      </RouterView>
+    </Suspense>
+    <FooterMain />
+  </div>
 </template>
+<script>
+import FooterMain from "./components/base/FooterMain.vue";
+import LoaderIcon from "./components/base/LoaderIcon.vue";
+import NavBar from "./components/base/NavBar.vue";
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+export default {
+  name: "app",
+  components: {
+    LoaderIcon,
+    FooterMain,
+    NavBar,
+  },
+  data() {
+    return {
+      windowWidth: null,
+      mobile: null,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      return;
+    },
+  },
+};
+</script>
