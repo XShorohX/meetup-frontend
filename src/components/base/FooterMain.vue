8c7e0a262d6c42636133e155a8fa8f5026226186
nspin <pinchuk.nik@yandex.ru>
2023-03-31 00:55:29 +0300

    Добавлен футер

---
+++ src/components/base/FooterMain.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed bottom-0 gradient w-full">
+    <p class="text-center">
+      © {{ new Date().getFullYear() }} АО "АЙОН". Все права защищены.
+    </p>
+  </div>
+</template>
